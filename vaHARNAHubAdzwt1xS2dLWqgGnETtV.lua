0d8fe3c50601b764106cf448d5e5384305022502
i-have-no-mouth-and-i-must-scream <cukak2008@gmail.com>
2026-03-21 19:50:54 +0100

    Update vaHARNAHubAdzwt1xS2dLWqgGnETtV.lua

--- vaHARNAHubAdzwt1xS2dLWqgGnETtV.lua
+++ vaHARNAHubAdzwt1xS2dLWqgGnETtV.lua
@@ -893,9 +893,9 @@ local Library do
 			return Font.new(getcustomasset(`{GetFolders().Assets}/{Name}.font`))
 		end
 
-		Library.Font = CustomFont:New("ProggyClean", 400, "Regular", {
-			Id = "ProggyClean",
-            Url = "https://github.com/LuckyHub1/LuckyHub/raw/main/ProggyClean.ttf"
+		Library.Font = CustomFont:New("Minecraftia", 400, "Regular", {
+			Id = "Minecraftia",
+            Url = "https://github.com/LuckyHub1/LuckyHub/raw/main/Minecraftia.ttf"
 		})
 	end
 
