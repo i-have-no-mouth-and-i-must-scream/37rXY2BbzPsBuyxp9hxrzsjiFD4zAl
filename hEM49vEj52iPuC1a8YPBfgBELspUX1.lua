d21d2fa6b8b73ade20277164df82bb69110e5f57
i-have-no-mouth-and-i-must-scream <cukak2008@gmail.com>
2026-05-03 23:22:49 +0700

    Add files via upload

--- hEM49vEj52iPuC1a8YPBfgBELspUX1.lua
+++ hEM49vEj52iPuC1a8YPBfgBELspUX1.lua
@@ -28,7 +28,7 @@ function Webhook.CreateMessage(Url, Username, Content)
 			description = description or "",
 			fields = {},
 			thumbnail = {url = "https://cdn.discordapp.com/attachments/1366160415444439160/1450846645045694474/solix_logo-min_1.png"},
-			footer = {text = "solixhub.com"},
+			footer = {text = "discord.gg/solixhub"},
 			timestamp = os.date("!%Y-%m-%dT%H:%M:%SZ")
 		}
 
@@ -50,4 +50,6 @@ function Webhook.CreateMessage(Url, Username, Content)
 	end
 	return webhook
 end
+
+getgenv().Webhook = Webhook
 return Webhook
